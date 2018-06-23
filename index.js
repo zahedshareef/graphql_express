9ac15162d4a7cf6a42e6ec313dd4d6405caf24be
Zahed Shareef <realzahed@live.com>
2018-06-24 01:03:28 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+const app = express();
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: 'graphql'
+}));
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.listen(3000);
