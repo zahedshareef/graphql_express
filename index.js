a16fa0c27504a6fa7aac5d3b21d3c56302362776
Zahed Shareef <realzahed@live.com>
2018-06-24 23:40:15 +0300

    add user authentication and authorization

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import { graphiqlExpress, graphqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
@@ -13,18 +14,36 @@ const schema = makeExecutableSchema({
     resolvers
 });
 
+const SECRET = "DlfBdXiwG2qqA6FNyK5pxviknyeaqN0rHo07HtAC59dbrTUS0";
+
 const app = express();
+
+const addUser = async (req) => {
+    const token = req.headers.authorization;
+    try {
+        const { user } = await jwt.verify(token, SECRET);
+        req.user = user;
+    } catch (err) {
+        console.log(err);
+    }
+    req.next();
+}
+
 app.use(cors('*'));
+app.use(addUser);
 
 app.use('/graphiql', graphiqlExpress({
     endpointURL: 'graphql'
 }));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({
+app.use('/graphql', bodyParser.json(), graphqlExpress(req => ({
     schema,
     context: {
-        models
-    }
-}));
+        models,
+        SECRET,
+        user: req.user,
+        },
+    })),
+);
 
 models.sequelize.sync().then(() => app.listen(3000));
--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,7 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import _ from 'lodash';
+
 export default {
     User: {
         boards: ({ id }, args, { models }) => models.Board.findAll({
@@ -28,12 +32,19 @@ export default {
     },
     Query: {
         allUsers: (parent, args, { models }) => models.User.findAll(),
-        getUser: (parent, { username }, { models }) => 
-        models.User.findOne({
-            where: {
-                username,
-            },
-        }),
+        me: (parent, args, { models, user }) => {
+            if(user) {
+                //user logged in
+                return models.User.findOne({
+                    where: {
+                        id: user.id,
+                    },
+                });
+            }
+            
+            //not logged in 
+            return null;
+        },
         userBoards: (parent, { owner }, { models }) => 
         models.Board.findAll({
             where: {
@@ -49,10 +60,27 @@ export default {
     },
 
     Mutation: {
-        createUser: (parent, args, { models }) => models.User.create(args), 
         updateUser: (parent, { username, newUsername }, { models }) => models.User.update({ username: newUsername}, { where: { username }}), 
         deleteUser: (parent, args, { models }) => models.User.destroy({where: args}), 
         createBoard: (parent, args, { models }) => models.Board.create(args),
-        createSuggestion: (parent, args, { models }) => models.Suggestion.create(args)
+        createSuggestion: (parent, args, { models }) => models.Suggestion.create(args),
+        register: async (parent, args, { models }) => {
+            const user = args;
+            user.password = await bcrypt.hash(user.password, 12);
+            return models.User.create(user);
+        }, 
+        login: async (parent, { email, password }, { models, SECRET }) => {
+            const user = await models.User.findOne({ where: { email } });
+            if(!user) {
+                throw new Error('No user found with $email');
+            }
+
+            const valid = await bcrypt.compare(password, user.password);
+            if(!valid) {
+                throw new Error('Incorrect password');
+            }
+
+            return jwt.sign({ user: _.pick(user, ['id', 'username']) }, SECRET, { expiresIn: '1y' });
+        }
     }
 };
